bc36e2e58401cb891a365feb9c9f5a2d6b4ff968
mnoorcahyo <mnoorcahyo@gmail.com>
2018-11-18 06:50:41 +0700

    react part1

---
+++ src/components/Contact.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact Page</h4>
+      <p>
+        Lorem Ipsum bukanlah teks-teks yang diacak. Ia berakar dari sebuah
+        naskah sastra latin klasik dari era 45 sebelum masehi, hingga bisa
+        dipastikan usianya telah mencapai lebih dari 2000 tahun. Richard
+        McClintock, seorang professor Bahasa Latin dari Hampden-Sidney College
+        di Virginia, mencoba mencari makna salah satu kata latin yang dianggap
+        paling tidak jelas, yakni consectetur, yang diambil dari salah satu
+        bagian Lorem Ipsum. Setelah ia mencari maknanya di di literatur klasik,
+        ia mendapatkan sebuah sumber yang tidak bisa diragukan. Lorem Ipsum
+        berasal dari bagian 1.10.32 dan 1.10.33 dari naskah "de Finibus Bonorum
+        et Malorum" (Sisi Ekstrim dari Kebaikan dan Kejahatan) karya Cicero,
+        yang ditulis pada tahun 45 sebelum masehi. BUku ini adalah risalah dari
+        teori etika yang sangat terkenal pada masa Renaissance. Baris pertama
+        dari Lorem Ipsum, "Lorem ipsum dolor sit amet..", berasal dari sebuah
+        baris di bagian 1.10.32.
+      </p>
+    </div>
+  );
+};
+export default Contact;
