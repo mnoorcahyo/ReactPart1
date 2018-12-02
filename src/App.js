aa3dd7caef5a4b5e4bfc11f5d4fbf2f238bc125c
mnoorcahyo <mnoorcahyo@gmail.com>
2018-12-02 16:36:22 +0700

    switch tag and importing image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Post from "./components/Post"
+import Post from "./components/Post";
 
 class App extends Component {
   render() {
@@ -12,10 +12,12 @@ class App extends Component {
       <Router>
         <div className="App">
           <Navbar />
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/:post_id" component={Post} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/:post_id" component={Post} />
+          </Switch>
         </div>
       </Router>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import {Link, NavLink, withRouter} from 'react-router-dom'
+import React from "react";
+import { Link, NavLink, withRouter } from "react-router-dom";
 
-const Navbar = (props) => {
-    // setTimeout(()=>{
-    //     props.history.push('/about')
-    // }, 2000)
-    return(
+const Navbar = props => {
+
+  return (
     <nav className="nav-wrapper red darken-3">
-        <div className="container">
-            <a className="brand-logo">Poke's Time</a>
-            <ul className="right">
-                <li><Link to ="/">Home</Link></li>
-                <li><NavLink to="/about">About</NavLink></li>
-                <li><NavLink to="/contact">Contact</NavLink></li>
-            </ul>
-        </div>
+      <div className="container">
+        <a className="brand-logo">Poke's Time</a>
+        <ul className="right">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
-    )
-}
-export default withRouter(Navbar)
+  );
+};
+export default withRouter(Navbar);
