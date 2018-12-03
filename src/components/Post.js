14c9e6e0d1838976ad197895369c3f7538e2ce26
mnoorcahyo <mnoorcahyo@gmail.com>
2018-12-04 06:33:10 +0700

    redux react-redux

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,31 +1,16 @@
 import React, { Component } from "react";
-import axios from 'axios'
+import {connect} from 'react-redux'
 
 
 class Post extends Component {
-  state = {
-    post: null
-  };
-
-  componentDidMount() {
-    let id = this.props.match.params.post_id;
-    axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
-        .then(res =>{
-            this.setState({
-                post: res.data
-            })
-        })
-  }
-
   render() {
-
-    const post = this.state.post ? (
+    const post = this.props.post ? (
         <div className="post">
             <h4 className="center">
-            {this.state.post.title}
+            {this.props.post.title}
             </h4>
             <p>
-            {this.state.post.body}
+            {this.props.post.body}
             </p>
         </div>
     ) : (
@@ -42,4 +27,11 @@ class Post extends Component {
   }
 }
 
-export default Post;
+const mapStateProps = (state, ownProps) => {
+let id = ownProps.match.params.post_id
+    return {
+        post: state.posts.find(post=> post.id === id)
+    }
+}
+
+export default connect(mapStateProps)(Post);
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,14 @@
+const initState = {
+    posts:[
+        {id:'1', title:'Squirtle Laid and Egg', body:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+        {id:'2', title:'Charmander Laid and Egg', body:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+        {id:'3', title:'A Helix Fossil was found', body:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+
+    ]
+}
+
+const rootReducer = (state = initState, action) =>{
+    return state
+}
+
+export default rootReducer;
